032f27d128fa2eb050e03bb8523bbc18a44c8dbf
SPARATA <ahmedsoudani09@gmail.com>
2024-06-05 10:06:28 +0100

    start with APIs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  /* whenever this class gets loaded run this constructor first */
+  constructor() {
+    /* call the underlying constructor method of any other class you're extending from */
+    super();
+
+    /* this references to this class */
+    this.state = {
+      /* inside of the state you enter a JSON format */
+      monsters : []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      response.json();
+    })
+    .then((users) => console.log(users))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+          );
+        })}
+      </div>
+    );
+  };
+  
 }
 
 export default App;
