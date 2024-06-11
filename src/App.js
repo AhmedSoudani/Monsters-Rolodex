2c7d702cc03a755c412c21710d8afa0e50d07c17
SPARATA <ahmedsoudani09@gmail.com>
2024-06-11 10:05:28 +0100

    added a little bit of css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -55,18 +57,18 @@ class App extends Component {
     console.log(filteredMonsters);
     return (
       <div className="App">
-        <input 
-        className='search-box' 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={onSearchChange} />
-        {filteredMonsters.map((monster) => {
+        <SearchBox 
+        onChangeHandler={onSearchChange} 
+        placeholder='Search Monsters'
+        className='monsters-search-box' />
+        {/* {filteredMonsters.map((monster) => {
           return (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
           );
-        })}
+        })} */}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   };
--- src/components/card-list.component.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class CardList extends Component {
-    render() {
-        return <div>Hello i'm the CardList Component</div>
-    }
-}
-
-export default CardList;
