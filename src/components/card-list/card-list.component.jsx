c2485061ce5346e0c4bec99ead880bf3f90a9238
SPARATA <ahmedsoudani09@gmail.com>
2024-06-11 10:45:51 +0100

    finishing css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,26 @@
 import { Component } from "react";
+import "./card-list.styles.css"
+
 
 class CardList extends Component {
     render() {
         const {monsters} = this.props;
+        console.log(monsters);
 
         return (
-        <div>
-            {monsters.map((monster) => (
-                <h1 key={monster.id}>{monsters.name}</h1>
-            ))}
+        <div className="card-list">
+            {monsters.map((monster) => { 
+
+                const { name, email, id} = monster
+
+                return (
+                <div key={id} className="card-container">
+                    <img src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                     alt={`monsters-${name}`} />
+                     <h2>{name}</h2>
+                     <p>{email}</p>
+                </div>
+            )})}
         </div>    
     )}
 
